bd295b39f236a328be660658c6b9bb8a6803f183
John Loyd Mulit <jlmulit68@gmail.com>
2023-10-01 16:04:32 +0800

    feat: move helper functions definition out of helpers.h

---
+++ helpers.cpp
@@ -0,0 +1,84 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> constructWords(const std::string &filename) {
+  std::string str;
+  std::vector<std::string> words;
+  std::ifstream reader;
+  std::string base = "./categories/";
+  reader.open(base.append(filename));
+
+  while (std::getline(reader, str)) {
+    words.push_back(str);
+  }
+
+  return words;
+}
+
+std::string generateRandomWord(std::vector<std::string> words) {
+  int bounds = words.size();
+  int secondsElapsed = time(0);
+  srand(secondsElapsed);
+
+  int random = rand() % bounds;
+
+  return words[random];
+}
+
+bool hasLetter(std::string &word, char &letter) {
+  for (int i = 0; i < word.size(); ++i) {
+    if (letter == word[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool alreadyGuessed(std::vector<char> &guesses, char &letter) {
+  for (int i = 0; i < guesses.size(); ++i) {
+    if (letter == guesses[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+std::string reveal(std::string toReveal, std::string &word, char &letter) {
+  for (int i = 0; i < word.size(); ++i) {
+    if (letter == word[i]) {
+      toReveal[i] = letter;
+    }
+  }
+  return toReveal;
+}
+
+void printToReveal(std::string &toReveal, bool withSpaces = true) {
+  for (int i = 0; i < toReveal.size(); ++i) {
+    if (withSpaces) {
+      std::cout << toReveal[i] << " ";
+    } else {
+      std::cout << toReveal[i];
+    }
+  }
+  std::cout << std::endl;
+}
+
+bool haveWon(std::string &toReveal) {
+  for (int i = 0; i < toReveal.size(); ++i) {
+    if (toReveal[i] == '_') {
+      return false;
+    }
+  }
+  return true;
+}
+
+std::string initGuesses(int size) {
+  std::string str;
+
+  for (int i = 0; i < size; ++i) {
+    str.push_back('_');
+  }
+
+  return str;
+}
--- helpers.h
+++ helpers.h
@@ -4,83 +4,18 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> constructWords(const std::string &filename) {
-  std::string str;
-  std::vector<std::string> words;
-  std::ifstream reader;
-  std::string base = "./categories/";
-  reader.open(base.append(filename));
+std::vector<std::string> constructWords(const std::string &);
 
-  while (std::getline(reader, str)) {
-    words.push_back(str);
-  }
+std::string generateRandomWord(std::vector<std::string> words);
 
-  return words;
-}
+bool hasLetter(std::string &, char &);
 
-std::string generateRandomWord(std::vector<std::string> words) {
-  int bounds = words.size();
-  int secondsElapsed = time(0);
-  srand(secondsElapsed);
+bool alreadyGuessed(std::vector<char> &, char &);
 
-  int random = rand() % bounds;
+std::string reveal(std::string, std::string &, char &);
 
-  return words[random];
-}
+void printToReveal(std::string &, bool withSpaces = true);
 
-bool hasLetter(std::string &word, char &letter) {
-  for (int i = 0; i < word.size(); ++i) {
-    if (letter == word[i]) {
-      return true;
-    }
-  }
-  return false;
-}
+bool haveWon(std::string &);
 
-bool alreadyGuessed(std::vector<char> &guesses, char &letter) {
-  for (int i = 0; i < guesses.size(); ++i) {
-    if (letter == guesses[i]) {
-      return true;
-    }
-  }
-  return false;
-}
-
-std::string reveal(std::string toReveal, std::string &word, char &letter) {
-  for (int i = 0; i < word.size(); ++i) {
-    if (letter == word[i]) {
-      toReveal[i] = letter;
-    }
-  }
-  return toReveal;
-}
-
-void printToReveal(std::string &toReveal, bool withSpaces = true) {
-  for (int i = 0; i < toReveal.size(); ++i) {
-    if (withSpaces) {
-      std::cout << toReveal[i] << " ";
-    } else {
-      std::cout << toReveal[i];
-    }
-  }
-  std::cout << std::endl;
-}
-
-bool haveWon(std::string &toReveal) {
-  for (int i = 0; i < toReveal.size(); ++i) {
-    if (toReveal[i] == '_') {
-      return false;
-    }
-  }
-  return true;
-}
-
-std::string initGuesses(int size) {
-  std::string str;
-
-  for (int i = 0; i < size; ++i) {
-    str.push_back('_');
-  }
-
-  return str;
-}
+std::string initGuesses(int size);
